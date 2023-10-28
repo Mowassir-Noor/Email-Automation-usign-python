e99ec7d382b3e958d67d83bf1f9faaedd98e6e13
Mowassir-Noor <mowassirnoor05@gmail.com>
2023-10-29 02:49:31 +0300

    Create email automation.py

---
+++ email automation.py
@@ -0,0 +1,33 @@
+import smtplib
+import ssl
+from email.message import EmailMessage
+
+
+def mail_automation():    
+        email_sender = 'sobannoor.testing@gmail.com'
+        email_password = 'qypp wuzn ljrt cdsk'   #using app password because actual pass doesn't get the access (google ki backchodi hai bas)
+        email_receiver = 'mowassirnoor05@gmail.com'
+
+        subject = 'Testing gmail automation'
+
+        body = """     main message  """
+
+
+        em = EmailMessage()
+        em['From'] = email_sender
+        em['To'] = email_receiver
+        em['Subject'] = subject
+        em.set_content(body)
+
+        context = ssl.create_default_context()
+
+
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+              #loging in to the sender's mail account
+            server.login(email_sender, email_password)
+                   #sending mail
+            server.send_message(em)
+
+if __name__=="__main__":
+      
+    mail_automation()
